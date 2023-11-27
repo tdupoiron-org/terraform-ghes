fbd19d4512f44f7a72ec63e5d618e7e2e83e030e
Thomas Dupoiron <tdupoiron@github.com>
2023-11-27 16:07:08 +0000

    Refactor AWS DNS configuration

--- aws/network.tf
+++ aws/network.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "ghes_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   tags = {
     Name  = "${var.owner}-ghes-vpc-tf"
     Owner = var.owner
--- aws/outputs.tf
+++ aws/outputs.tf
@@ -2,8 +2,8 @@ output "ghes_ec2_public_ip" {
   value = aws_instance.ghes_ec2.public_ip
 }
 
-output "ghes_dns_record_fqdn" {
-  value = "${aws_route53_record.ghes_dnsrecord.name}.${aws_route53_zone.ghes_dnszone_public.name}"
+output "ghes_ec2_public_dns" {
+  value = aws_instance.ghes_ec2.public_dns
 }
 
 output "ghes_setup_endpoint" {
