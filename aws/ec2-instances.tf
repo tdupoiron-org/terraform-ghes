da8b352ad1bf4f09bfe90b819c8e07b9e6010008
Thomas Dupoiron <tdupoiron@github.com>
2024-03-06 08:05:15 +0000

    Add runner ec2 instance

--- aws/ec2-instances.tf
+++ aws/ec2-instances.tf
@@ -52,3 +52,55 @@ resource "aws_volume_attachment" "ghes_ebs_attachment_data" {
   volume_id   = aws_ebs_volume.ghes_ebs_data.id
   instance_id = aws_instance.ghes_ec2.id
 }
+
+# RUNNERS
+data "aws_ami" "amazon_linux_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+}
+
+resource "aws_instance" "runner_ec2" {
+  ami           = data.aws_ami.amazon_linux_ami.id
+  instance_type = var.aws_instance_type
+  # key_name      = aws_key_pair.ghes_kp.key_name
+  availability_zone           = var.aws_availability_zones[0]
+  associate_public_ip_address = false
+  subnet_id                   = aws_subnet.ghes_subnets[0].id
+  vpc_security_group_ids      = [aws_security_group.ghes_sg.id]
+
+  root_block_device {
+    volume_size = var.root_volume_size
+    tags = {
+      Name  = "${var.owner}-runner-ebs-root-tf"
+      Owner = var.owner
+    }
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name  = "${var.owner}-runner-ec2-tf"
+    Owner = var.owner
+  }
+
+}
